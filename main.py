9a2049e85ee9664f16b455e01f1a99771d045aa0
Gergely Máté <gergelymate@Gergelys-MacBook-Pro.local>
2016-07-17 22:47:31 +0200

    py file added

---
+++ main.py
@@ -0,0 +1,27 @@
+from math import *
+# TASK: quadratic formula solver
+
+# Example 1
+# ax^2 + bx + c = 0
+# a = 2  - input
+# b = -3 - input
+# c = 1  - input
+# Equation: 1x^2 + 2x + 3 = 0. Press ENTER to start - input
+# Result: x_1 = 1.0000; x_2 = 0.5000;
+
+# Example 2
+# a = 1 - input
+# b = 2 - input
+# c = 1 - input
+# Equation: 1x^2 + 2x + 3 = 0. Press ENTER to start - input
+# Result: x = -1.0000;
+
+# Example 3
+# Result: equation can not be solved
+
+def main():
+    print('test')
+    return
+
+if __name__ == '__main__':
+    main()
