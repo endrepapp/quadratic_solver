35122a507c8d74936c1d34480f07e7f9ac9e466f
Gergely Máté <gergelymate@Gergelys-MacBook-Pro.local>
2016-07-22 21:07:56 +0200

    solution added

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#  Ez azért kell, mert így elfogadja az ékezetet. UTF-8-as betűkódolásra váltottunk
 
+from math import *
 # TASK: quadratic formula solver
 
 # Example 1
 # ax^2 + bx + c = 0
-# a = 2  - input
+# a = 2 - input
 # b = -3 - input
-# c = 1  - input
+# c = 1 - input
 # Equation: 2x^2 - 3x + 1 = 0. Press ENTER to start - input
 # Result: x_1 = 1.0000; x_2 = 0.5000;
 
@@ -21,74 +23,55 @@
 # Example 3
 # Result: equation can not be solved
 
-from math import *
-
-# def function(input):
-#     output = input
-#     return output
-
-def szam_beker(betu = "a"):
-    return int(input("Kerem adja meg az {} - erteket: ".format(betu)))
-
 def main():
-    print "udvozlom az ekezet nelkuli fuggveny megoldoban."
-
-    a = szam_beker()
+    print 'ax^2 + bx + c = 0'
+    a = szam_beker("a")
+    # ha a nem nagyobb mint 0, akkor nincs értelme
+    if a == 0:
+        print "Ez nem másodfokú egyenlet!"
+        return
+    # a értékhez előjel kiíratás
+    elif a < 0:
+        s_a = "- {}".format(abs(a))
+    else:
+        s_a = a
     b = szam_beker("b")
     c = szam_beker("c")
-    
-    # TODO: Teljes egyenlet kiíratása
-    # Equation: 2x^2 - 3x + 1 = 0. Press ENTER to start
 
-    # TODO: legyen benne determináns kiszámoló függvény
-    # TODO: Helyesn számolja ki a függvényt (WolframAplha)
-
-    # a változók elnevezését cseréld ki valósra
-    szam1 = int
-    szam2 = int
-    szam3 = int
-    szam4 = int
-    szam5 = int
-    szam6 = int
-    szam7 = int
-    szam8 = int
-    szam9 = int
-    szam10 = int
-    szam11 = int
-    szam12 = int
-
-    if(a==0&b==0&c==0):
-        # TODO: ezt a részt töröld
-        print ("minek kezdett bele?! ") 
+    # b, c értékekhez előjel kiíratás
+    if b < 0:
+        s_b = "- {}".format(abs(b))
     else:
-        szam1 = b * (-1)
-        szam2 = b * b
-        szam3 = 4 * a * c
-        szam4 = (szam2) - (szam3)
-    if(szam4<0):
-        print "gyok alatt a szammok fosast eredmenyeztek"
-        # TODO: Result: equation can not be solved
-
+        s_b = "+ {}".format(b)
+    if c < 0:
+        s_c = "- {}".format(abs(c))
     else:
-    #minuszos----------------
-        szam5 = sqrt(szam4)
-        szam6 = szam1 - szam5
-        szam7 = 2 * a
-        szam8 = szam6 / szam7
-    #pluszos-----------------
-        szam10 = sqrt(szam4)
-        szam11 = szam1+ szam10
-        szam12 = szam10/szam7
+        s_c = "+ {}".format(c)
 
-        if(szam8==0):
-            print "nop"
-        else:
-            print "x1: %s." % (szam8)
-        if(szam12 == 0):
-            print "nop"
-        else:
-            print "x2: %s." % (szam12)
+    # az egyenlet kiíratásánál figyelni kell az előjelre
+    raw_input("Equation: {}x^2 {}x {} = 0. Press ENTER to start".format(s_a,s_b,s_c))
+
+    det = determinans(a,b,c)
+    if det < 0:
+        # ha nincs megoldása az egyenletnek, ki is lépünk
+        print "Result: equation can not be solved"
+        return # kilépés
+    elif det == 0:
+        # ha csak egy megoldása van
+        x = - b / (2 * a)
+        print "Result: x = {}".format(x)
+    else:
+        # ha két megoldása van
+        x_1 = (-b + sqrt(det)) / (2 * a)
+        x_2 = (-b - sqrt(det)) / (2 * a)
+        print "Result: x_1 = {}; x_2 = {};".format(x_1, x_2)
     return
+
+def szam_beker(betu):
+    return int(raw_input("{} = ".format(betu)))
+
+def determinans(a,b,c):
+    return b * b - 4 * a * c
 
 if __name__ == '__main__':
     main()
